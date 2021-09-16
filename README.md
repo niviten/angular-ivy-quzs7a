81d6071f496baf6570c6d7b718d4a2fc31f65b16
niviten <l.j.nivethan@gmail.com>
2021-09-16 22:51:53 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-quzs7a
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-quzs7a)
---
+++ src/app/nivi/nivi.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nivi',
+  templateUrl: './nivi.component.html',
+  styleUrls: ['./nivi.component.css']
+})
+export class NiviComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
